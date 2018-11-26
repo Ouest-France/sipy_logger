2d028b30e9805ecfddf230122b5a3a4fc9a87e62
Luc Sorel-Giffo <lucsorel@users.noreply.github.com>
2018-11-26 18:56:16 +0100

    added graypy dep (#3)

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ with open('README.md', 'r') as fh:
     longDescription = fh.read()
 
 setuptools.setup(name='sipy_logger',
-    version='0.1.0',
+    version='0.1.1',
     description='Programmatically creates python loggers with different handlers (console, file, graylog)',
     long_description=longDescription,
     long_description_content_type="text/markdown",
     author='Ouest-France/SIPA Tech',
     url='https://github.com/Ouest-France/sipy_logger',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'graypy',
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
--- sipy_logger/__init__.py
+++ sipy_logger/__init__.py
@@ -6,4 +6,4 @@ Programmatically creates python loggers with different handlers (console, file, 
 
 from .getLogger import getLogger
 
-__version__ = (0, 1, 0)
+__version__ = (0, 1, 1)
